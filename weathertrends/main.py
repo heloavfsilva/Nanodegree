5e9c3b02046f76401252a85e57caca90ad6fb6af
Heloa Silva <hsilva25@its.jnj.com>
2019-05-26 10:03:30 -0300

    add the global data to line chart

--- weathertrends/main.py
+++ weathertrends/main.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from numpy import NaN
+from datetime import datetime
 
 # Import the files to be used
-city_data = pd.read_csv("city_data.csv", parse_dates=["year"], index_col=["year"])
+city_data = pd.read_csv("city_data.csv", index_col=["year"])
 #print(city_data.head())
 
 city_list = pd.read_csv("city_list.csv")
 #print(city_list.head())
 
 global_data = pd.read_csv("global_data.csv")
-#print(global_data.head())
+# print(global_data.head())
 
 #Filter the Brazil cities list
 isBrazil = city_list['country'] == 'Brazil'
@@ -19,24 +20,34 @@ city_list_br = city_list[isBrazil]
 # Filter the all city data in Brazil
 city_data_br = city_data['city']== 'Belo Horizonte'
 city_data_br = city_data[city_data_br]
-#print(city_data_br.head())
+# print(city_data_br.head())
+# city_br_mva = pd.Series(city_data_br)
 
-#city_br_mva = pd.Series(city_data_br)
+year = 3
+mva_global = global_data
+mva_global = mva_global.replace(0, NaN)
+mva_global = mva_global.dropna(how='all', axis=0)
+mva_global = global_data.rolling(year).mean()
+# print(mva_global)
 
-mva = city_data_br.drop(['city', 'country'], axis=1)
-mva= mva.replace(0, NaN)
-mva=mva.dropna(how='all', axis=0)
-mva7 = mva.rolling(7).mean()
-mva5 = mva.rolling(5).mean()
-mva2 = mva.rolling(2).mean()
+mva_local = city_data_br.drop(['city', 'country'], axis=1)
+mva_local = mva_local.replace(0, NaN)
+mva_local = mva_local.dropna(how='all', axis=0)
+mva_local = mva_local.rolling(year).mean()
 
-# print(mva)
+# print(mva_local);
+
+df = pd.merge(mva_local, mva_global, on='year')
+print(df)
+
 fig = plt.figure()
-plt.plot(mva2)
+plt.plot('year', 'avg_temp_x', data=df, marker='', color='skyblue', linewidth=2, label="Local")
+plt.plot('year', 'avg_temp_y', data=df, marker='', color='olive', linewidth=2, linestyle='dashed', label="Global")
+
 plt.xlabel('year', fontsize=18)
 plt.ylabel('degree', fontsize=16)
-plt.title('Weather trends for Belo Horizonte in 2 years avg')
-fig.savefig('BH-2years.jpg')
+plt.title('Weather trends for Belo Horizonte in '+str(year)+' years avg')
+fig.savefig('BH-'+str(year)+'years.png')
 plt.show()
 
 
